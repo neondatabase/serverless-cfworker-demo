0332d8094b3b770d4ed7a80e214b8708fb9e9442
George MacKerron <george@mackerron.co.uk>
2022-11-02 15:56:49 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Client, neonConfig } from '@jawj/tmp-cfworker-pg';
+
+interface Env { DATABASE_URL: string }
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const cf = request.cf ?? {} as any;
+    const lat: number = parseFloat(cf.latitude ?? '37.818496');
+    const lng: number = parseFloat(cf.longitude ?? '-122.473831');
+    const city: string = cf.city ?? 'Unknown location (assuming San Francisco)';
+    const country: string = cf.country ?? 'Earth';
+
+    neonConfig.disableSNI = true;
+    // neonConfig.wsProxy = 'localhost:9999';
+
+    const client = new Client({ connectionString: env.DATABASE_URL });
+    await client.connect();
+
+    const { rows } = await client.query(`
+      select 
+        id_no, name_en, category,
+        st_setsrid(st_makepoint($1, $2), 4326)::geography <-> location as distance
+      from whc_sites_2021
+      order by distance limit 10`,
+      [lng, lat]
+    );
+
+    ctx.waitUntil(client.end());
+
+    return new Response(
+      JSON.stringify({ lat, lng, city, country, nearestSites: rows }, null, 2),
+      { headers: { 'Content-Type': 'application/json' } },
+    );
+  }
+}
